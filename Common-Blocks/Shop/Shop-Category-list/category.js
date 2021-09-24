3f64f7d72e356d8912ac9d75e65a7c63d8dd763c
AndreasHjorth11 <77662628+AndreasHjorth11@users.noreply.github.com>
2021-09-24 13:12:10 +0200

    Add Shop Changes

--- Common-Blocks/Shop/Shop-Category-list/category.js
+++ Common-Blocks/Shop/Shop-Category-list/category.js
@@ -1,12 +1,26 @@
+let url = window.location.search;
+let params = new URLSearchParams(url);
+let category = params.get("category")
+console.log(category);
 
 let widebarTitle = document.querySelector("#widebar__title");
+let itemCategory = document.querySelector("#item__category");
 
 fetch('/Common-Blocks/Shop/shop.json')
 .then(response => response.json())
 .then(result => {
     result.shop.forEach(element =>{
+        if(element.id == category){
             widebarTitle.innerHTML = ` 
             <strong class="widebar__title">${element.title}</strong>
-        ` 
+            
+        `; 
+        }
+        
+        if(element.id == category){
+            itemCategory.innerHTML = `
+            <strong class="category__amplifiers">${element.title}</strong>
+            `;
+        }
     });
 });
